bdd92600e6fb1e09469f90de5d1b14320f3a282e
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-08-12 13:17:47 +0300

    There is no bash in dnsmasq container

--- dnsmasq/container/backpack-healthcheck.sh
+++ dnsmasq/container/backpack-healthcheck.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 nc -z -u -w 2 localhost 53 \
 && curl --head --fail --silent --user-agent healthcheck --user admin_inestorasyno:impardalwina http://localhost:8080/ > /dev/null
