fc48a7b5ec3078800139f11c1da19233addfe34d
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-08-14 10:56:58 +0300

    Play with Diun

--- .incubator/diun/container/simple-log-notifier.sh
+++ .incubator/diun/container/simple-log-notifier.sh
@@ -9,9 +9,9 @@ echo "Status:   $DIUN_ENTRY_STATUS"   >> $LOG
 echo "Created:  $DIUN_ENTRY_CREATED"  >> $LOG
 echo "Digest:   $DIUN_ENTRY_DIGEST"   >> $LOG
 # echo "MIME:     $DIUN_ENTRY_MIMETYPE" >> $LOG
-echo                                  >> $LOG
-echo "Host:     $DIUN_HOSTNAME"       >> $LOG
-echo "Diun:     $DIUN_VERSION"        >> $LOG
-echo "DB:       $DIUN_DB_PATH"        >> $LOG
-echo "Provider: $DIUN_ENTRY_PROVIDER" >> $LOG
+# echo                                  >> $LOG
+# echo "Host:     $DIUN_HOSTNAME"       >> $LOG
+# echo "Diun:     $DIUN_VERSION"        >> $LOG
+# echo "DB:       $DIUN_DB_PATH"        >> $LOG
+# echo "Provider: $DIUN_ENTRY_PROVIDER" >> $LOG
 echo "------------------------------" >> $LOG
