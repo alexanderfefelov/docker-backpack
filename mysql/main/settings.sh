66ba71d3a3bba5e85fe1a7df991e25ba4a642c18
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-08-15 22:45:25 +0300

    Fix containers name

--- mysql/main/settings.sh
+++ mysql/main/settings.sh
@@ -3,9 +3,9 @@ readonly IMAGE_NAME=alexanderfefelov/$BASE_NAME
 readonly MASTER_CONTAINER_NAME=$BASE_NAME-master
 readonly BACKUP_CONTAINER_NAME=$BASE_NAME-backup
 readonly SLAVE_CONTAINER_NAME=$BASE_NAME-slave
-readonly MASTER_HOST_NAME=$BASE_NAME
-readonly BACKUP_HOST_NAME=$BASE_NAME
-readonly SLAVE_HOST_NAME=$BASE_NAME
+readonly MASTER_HOST_NAME=$BASE_NAME-master
+readonly BACKUP_HOST_NAME=$BASE_NAME-backup
+readonly SLAVE_HOST_NAME=$BASE_NAME-slave
 readonly WAIT_TIMEOUT=600
 
 . ../../common-settings/health.sh
