9277c9fff2259e466176e67db3d6e9d4fda3d191
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-08-27 07:16:39 +0300

    Minor improvement

--- lib/lib.sh
+++ lib/lib.sh
@@ -1,5 +1,8 @@
 print_container_info() {
-  echo $1 is ready at $(docker inspect --format '{{ .NetworkSettings.IPAddress }}' $1)
+  echo $1
+  echo -e "\tIP\t$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' $1)"
+  echo -e "\tPorts\t$(docker inspect --format '{{ .NetworkSettings.Ports }}' $1)"
+  echo -e "\tMounts\t$(docker inspect --format '{{ .Mounts }}' $1)"
 }
 
 wait_for_all_container_ports() {
