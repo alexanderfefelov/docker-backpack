dc64f75b406a4bca3eb40783a7ef1060e885cf33
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-08-27 20:03:48 +0300

    Minor improvements

--- lib/lib.sh
+++ lib/lib.sh
@@ -1,8 +1,8 @@
 print_container_info() {
   echo $1
-  echo -e "\tIP\t$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' $1)"
-  echo -e "\tPorts\t$(docker inspect --format '{{ .NetworkSettings.Ports }}' $1)"
-  echo -e "\tMounts\t$(docker inspect --format '{{ .Mounts }}' $1)"
+  echo -e "\tIP address\n$(docker inspect --format '{{printf "\t\t%s/%d" .NetworkSettings.IPAddress .NetworkSettings.IPPrefixLen}}' $1)"
+  echo -e "\tPorts\n$(docker inspect --format '{{range $key, $value := .NetworkSettings.Ports }}{{printf "\t\t"}}{{if $value}}{{printf "%s:%s" (index $value 0).HostIp (index $value 0).HostPort}}{{else}}∅{{end}}{{printf " -> %s\n" $key}}{{end}}' $1)"
+  echo -e "\tMounts\n$(docker inspect --format '{{range $data := .Mounts }}{{printf "\t\t%s %s -> %s rw=%t\n" $data.Type $data.Source $data.Destination $data.RW}}{{end}}' $1)"
 }
 
 wait_for_all_container_ports() {
