cf882c1f132f0fd84fde659e4ff26624c48c8caa
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-08-27 20:10:21 +0300

    Minor improvements

--- lib/lib.sh
+++ lib/lib.sh
@@ -1,8 +1,11 @@
 print_container_info() {
-  echo $1
-  echo -e "\tIP address\n$(docker inspect --format '{{printf "\t\t%s/%d" .NetworkSettings.IPAddress .NetworkSettings.IPPrefixLen}}' $1)"
-  echo -e "\tPorts\n$(docker inspect --format '{{range $key, $value := .NetworkSettings.Ports }}{{printf "\t\t"}}{{if $value}}{{printf "%s:%s" (index $value 0).HostIp (index $value 0).HostPort}}{{else}}∅{{end}}{{printf " -> %s\n" $key}}{{end}}' $1)"
-  echo -e "\tMounts\n$(docker inspect --format '{{range $data := .Mounts }}{{printf "\t\t%s %s -> %s rw=%t\n" $data.Type $data.Source $data.Destination $data.RW}}{{end}}' $1)"
+  echo -e "\n$1"
+  echo -e "\n\tIP address"
+  docker inspect --format '{{printf "\t\t%s/%d" .NetworkSettings.IPAddress .NetworkSettings.IPPrefixLen}}' $1
+  echo -e "\n\tPorts"
+  docker inspect --format '{{range $key, $value := .NetworkSettings.Ports }}{{printf "\t\t"}}{{if $value}}{{printf "%s:%s" (index $value 0).HostIp (index $value 0).HostPort}}{{else}}∅{{end}}{{printf " -> %s\n" $key}}{{end}}' $1
+  echo -e "\tMounts"
+  docker inspect --format '{{range $data := .Mounts }}{{printf "\t\t%s %s -> %s rw=%t\n" $data.Type $data.Source $data.Destination $data.RW}}{{end}}' $1
 }
 
 wait_for_all_container_ports() {
