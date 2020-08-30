dee707d79b59b795f604cee25077ac21946c41ad
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-08-30 21:35:29 +0300

    WIP Jenkins

--- .playground/jenkins/run.sh
+++ .playground/jenkins/run.sh
@@ -16,7 +16,9 @@ run() {
     --hostname $HOST_NAME \
     --detach \
     --volume /etc/localtime:/etc/localtime:ro --volume /etc/timezone:/etc/timezone:ro \
+    --volume $CONTAINER_NAME-data:/var/jenkins_home \
     --publish 8083:8080 \
+    --publish 50000:50000 \
     $DEFAULT_HEALTH_SETTINGS \
     $DEFAULT_LOG_SETTINGS \
     $IMAGE_NAME
