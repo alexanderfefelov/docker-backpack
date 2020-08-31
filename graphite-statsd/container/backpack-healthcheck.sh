0c4328f6f6e12f35b9006b1bcebb572d115f7b59
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-08-31 06:34:14 +0300

    Fix healthcheck

--- graphite-statsd/container/backpack-healthcheck.sh
+++ graphite-statsd/container/backpack-healthcheck.sh
@@ -10,7 +10,7 @@ nc -z -w $WAIT_TIMEOUT localhost 2003
 nc -z -w $WAIT_TIMEOUT localhost 2004
 nc -z -w $WAIT_TIMEOUT localhost 2023
 nc -z -w $WAIT_TIMEOUT localhost 2024
-# TODO Health check exceeded timeout (2s) nc -z -w $WAIT_TIMEOUT -u localhost 8125
+nc -z -w $WAIT_TIMEOUT -u localhost 8125
 nc -z -w $WAIT_TIMEOUT localhost 8126
 curl --head --fail --silent --max-time $WAIT_TIMEOUT --user-agent "$(basename "$0")" http://localhost:80/ > /dev/null
 curl --head --fail --silent --max-time $WAIT_TIMEOUT --user-agent "$(basename "$0")" http://localhost:8080/ > /dev/null
