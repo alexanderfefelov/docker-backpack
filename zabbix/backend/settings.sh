8f8f3dc19d7a4f564941a05b132372c8c1d4dc9b
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-09-02 19:24:38 +0300

    Check health of Zabbix backend

--- zabbix/backend/settings.sh
+++ zabbix/backend/settings.sh
@@ -2,7 +2,7 @@ readonly BASE_NAME=backpack-zabbix-backend
 readonly IMAGE_NAME=alexanderfefelov/$BASE_NAME
 readonly CONTAINER_NAME=$BASE_NAME
 readonly HOST_NAME=$BASE_NAME
-readonly WAIT_TIMEOUT=1800
+readonly WAIT_TIMEOUT=4200
 
 readonly DB_HOST=master.mysql.backpack.test
 readonly DB_DATABASE=zabbix
