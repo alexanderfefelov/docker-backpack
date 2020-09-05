2fd5a9b019b50e7f0ddf923d978485e28b377db1
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-09-05 08:40:51 +0300

    Check health of NAV

---
+++ nav/container/backpack-healthcheck.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Exit immediately if a pipeline, which may consist of a single simple command,
+# a list, or a compound command returns a non-zero status
+set -e
+
+readonly WAIT_TIMEOUT=2
+
+[ $(supervisorctl status | grep --count --invert-match RUNNING) = 0 ]
+nc -z -N -w $WAIT_TIMEOUT -u localhost 162
+curl --head --fail --silent --max-time $WAIT_TIMEOUT --user-agent "$(basename "$0")" http://localhost:80/ > /dev/null
