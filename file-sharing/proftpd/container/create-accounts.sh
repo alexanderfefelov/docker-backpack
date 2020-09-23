41637fa60d1859100a7d2f2a580bc39f2cb7e767
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-09-23 04:36:43 +0300

    Fix typos

--- file-sharing/proftpd/container/create-accounts.sh
+++ file-sharing/proftpd/container/create-accounts.sh
@@ -4,4 +4,4 @@ while IFS=: read -r username password; do
   echo -n Creating account $username...
   useradd $username --password $(mkpasswd $password) --create-home
   echo done
-done < /create-acconts.data
+done < /create-accounts.data
