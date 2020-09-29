bb39d5719093dfa53d92faba3bd533c42cfc5548
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-09-29 12:50:58 +0300

    Add run-navdump script

---
+++ utils/nav/run-navdump.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Elevate privileges
+[ $UID -eq 0 ] || exec sudo bash "$0" "$@"
+
+docker exec --tty --interactive backpack-nav bash -c '
+  set -e
+  readonly DUMP_DIR=/var/lib/nav/navdump
+  echo Dump directory: $DUMP_DIR
+  read -p "Existing dump files will be overwritten. Press Y to continue: " -n 1 -r
+  echo
+  if [ "$REPLY" != "Y" ]; then
+    exit
+  fi
+  mkdir --parents $DUMP_DIR
+  cd $DUMP_DIR
+  navdump --all
+'
