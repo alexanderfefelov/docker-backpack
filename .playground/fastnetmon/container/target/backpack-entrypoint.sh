b24f4e1412a61981a4a85f16a7411b1bb804663a
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-10-05 21:22:10 +0300

    Fix typo

--- .playground/fastnetmon/container/target/backpack-entrypoint.sh
+++ .playground/fastnetmon/container/target/backpack-entrypoint.sh
@@ -6,7 +6,7 @@ readonly TEMPLATE_FILE=/fastnetmon.conf.template
 readonly GRAPHITE_IP_ADDRESS=$(getent hosts $GRAPHITE_HOST | awk '{ print $1 }')
 
 if [ ! -f $CONF_FILE ]; then
-  echo Generating $CONF_FIILE from $TEMPLATE_FILE...
+  echo Generating $CONF_FILE from $TEMPLATE_FILE...
   cat $TEMPLATE_FILE \
     | sed "s/_GRAPHITE_IP_ADDRESS_/$GRAPHITE_IP_ADDRESS/g" \
     | sed "s/_GRAPHITE_PORT_/$GRAPHITE_PORT/g" \
