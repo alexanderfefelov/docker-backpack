c5a8fd65de26946b1c54b445c4d4cc6eb978b4d2
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-10-11 21:11:03 +0300

    Clean the code up

--- guacamole/frontend/init/initialize-guacamole.sh
+++ guacamole/frontend/init/initialize-guacamole.sh
@@ -8,14 +8,13 @@ readonly HTTP="http --check-status"
 readonly API=http://guacamole-frontend.backpack.test:8085/guacamole/api
 readonly API_SESSION=$API/session/data/mysql
 readonly LOG_FILE=initialize-guacamole.log
-readonly TEMP_JSON_FILE=temp.json.generated
 
 log() {
   echo $1 >> $LOG_FILE
 }
 
-create_token() {
-  log "Requesting token for $1 / $2"
+authenticate() {
+  log "Authenticating $1 / $2"
   local response="$($HTTP --form --body POST $API/tokens username=$1 password=$2)"
   log "Response: $response"
   local token=$(echo $response | jq --raw-output .authToken)
@@ -23,32 +22,27 @@ create_token() {
   echo $token
 }
 
-delete_token() {
-  log "Deleting token $1"
+deauthenticate() {
+  log "Deauthenticating $1"
   $HTTP DELETE $API/tokens/$1
   log "Token deleted"
 }
 
 create_users() {
   log "Creating users, token: $1"
-  local IFS=$'\n'
   for obj in $(jq --compact-output .[] < users.json); do
-    local IFS=' '
-    echo $obj > $TEMP_JSON_FILE
-    $HTTP POST $API_SESSION/users token==$1 < $TEMP_JSON_FILE
-    rm --force $TEMP_JSON_FILE
+    $HTTP POST $API_SESSION/users token==$1 <<< "$obj"
   done
   log "Users created"
 }
 
 update_permissions() {
   log "Updating permissions, token: $1"
-  jq --compact-output --raw-output '.[] | .username, .permissions' < permissions.json | paste --delimiters='|' - - | \
-  while IFS='|' read -r username permissions; do
-    echo $permissions > $TEMP_JSON_FILE
-    $HTTP PATCH $API_SESSION/users/$username/permissions token==$1 < $TEMP_JSON_FILE
-    rm --force $TEMP_JSON_FILE
-  done
+  jq --compact-output --raw-output '.[] | .username, .permissions' < permissions.json \
+  | paste --delimiters='|' - - \
+  | while IFS='|' read -r username permissions; do
+      $HTTP PATCH $API_SESSION/users/$username/permissions token==$1 <<< "$permissions"
+    done
   log "Permissions updated"
 }
 
@@ -61,22 +55,18 @@ delete_default_admin() {
 
 create_connections() {
   log "Creating connections, token: $1"
-  local IFS=$'\n'
   for obj in $(jq --compact-output .[] < connections.json); do
-    local IFS=' '
-    echo $obj > $TEMP_JSON_FILE
-    $HTTP POST $API_SESSION/connections token==$1 < $TEMP_JSON_FILE
-    rm --force $TEMP_JSON_FILE
+    $HTTP POST $API_SESSION/connections token==$1 <<< "$obj"
   done
   log "Connections created"
 }
 
-token=$(create_token guacadmin guacadmin)
+token=$(authenticate guacadmin guacadmin)
 create_users $token
 update_permissions $token
-delete_token $token
+deauthenticate $token
 
-token=$(create_token admin_reuphoodeixu zaicieceifox)
+token=$(authenticate admin_reuphoodeixu zaicieceifox)
 delete_default_admin $token
 create_connections $token
-delete_token $token
+deauthenticate $token
