3a561af881dad39d7f20103b8bf784dac6c523ae
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-10-13 05:43:47 +0300

    Use template file

--- dnsmasq/build.sh
+++ dnsmasq/build.sh
@@ -9,89 +9,10 @@ set -e
 
 . settings.sh
 
-generate_config() {
-  cat > container/etc/dnsmasq.d/address.conf.generated << EOF
-# GENERATED. DO NOT EDIT!
-
-address=/host.backpack.test/$1
-
-address=/dnsmasq.backpack.test/$1
-address=/netdata.backpack.test/$1
-address=/portainer.backpack.test/$1
-
-address=/grafana.backpack.test/$1
-address=/graphite-statsd.backpack.test/$1
-address=/jenkins.backpack.test/$1
-address=/mattermost.backpack.test/$1
-address=/redis.backpack.test/$1
-
-# Business intelligence
-#
-address=/cubes-backend.backpack.test/$1
-address=/cubes-frontend.backpack.test/$1
-address=/metabase.backpack.test/$1
-
-# Databases
-#
-address=/mongodb-main.backpack.test/$1
-address=/mongodb-test.backpack.test/$1
-address=/mysql-main-backup.backpack.test/$1
-address=/mysql-main-master.backpack.test/$1
-address=/mysql-main-slave.backpack.test/$1
-address=/mysql-test.backpack.test/$1
-address=/postgresql-main.backpack.test/$1
-address=/postgresql-test.backpack.test/$1
-
-# Elastic stack
-#
-address=/apm.backpack.test/$1
-address=/elasticsearch.backpack.test/$1
-address=/kibana.backpack.test/$1
-address=/logstash.backpack.test/$1
-
-# File sharing
-#
-address=/proftpd.backpack.test/$1
-address=/samba.backpack.test/$1
-
-# Guacamole
-#
-address=/guacamole-backend.backpack.test/$1
-address=/guacamole-frontend.backpack.test/$1
-
-# InfluxData stack
-#
-address=/chronograf.backpack.test/$1
-address=/influxdb.backpack.test/$1
-address=/kapacitor.backpack.test/$1
-address=/telegraf.backpack.test/$1
-
-# Message queues
-#
-address=/activemq.backpack.test/$1
-address=/rabbitmq.backpack.test/$1
-
-# Network monitoring
-#
-address=/fastnetmon.backpack.test/$1
-address=/nav.backpack.test/$1
-address=/zabbix-backend.backpack.test/$1
-address=/zabbix-frontend.backpack.test/$1
-
-# Prometheus
-#
-address=/alertmanager.backpack.test/$1
-address=/prometheus.backpack.test/$1
-address=/pushgateway.backpack.test/$1
-
-# Workload automation
-#
-address=/dkron-server.backpack.test/$1
-address=/dkron-agent-1.backpack.test/$1
-address=/dkron-agent-2.backpack.test/$1
-address=/rundeck.backpack.test/$1
-
-EOF
+generate_config_files() {
+  echo Genarating config files...
+  envsubst < build/address.conf.template > container/etc/dnsmasq.d/address.conf.generated
+  echo ..config files generated
 }
 
 readonly IP_ADDRESS=$(ip route get 1.0.0.0 | awk '{ print $7 }')
@@ -100,5 +21,6 @@ if [ -z $IP_ADDRESS ]; then
   exit 1
 fi
 
-generate_config $IP_ADDRESS
+export IP_ADDRESS
+generate_config_files
 docker build --tag $IMAGE_NAME .
