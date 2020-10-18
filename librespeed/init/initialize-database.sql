bcb7a5999e3ce45e02786b5949d5135ae088bbce
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-10-18 11:30:07 +0300

    Improve the IP address storage

--- librespeed/init/initialize-database.sql
+++ librespeed/init/initialize-database.sql
@@ -1,20 +1,21 @@
 CREATE DATABASE $DB_DATABASE;
 USE $DB_DATABASE;
 CREATE TABLE speedtest_users( -- Based on https://github.com/librespeed/speedtest-go/blob/master/database/mysql/telemetry_mysql.sql
-  id        int(11)   NOT NULL AUTO_INCREMENT,
-  timestamp timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  ip        tinytext  NOT NULL, -- FIXME
+  id        int(11)     NOT NULL AUTO_INCREMENT,
+  timestamp timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  ip        varchar(15) NOT NULL,
   ispinfo   text,
   extra     text,
   ua        text,
   lang      tinytext,
-  dl        float     NOT NULL,
-  ul        float     NOT NULL,
-  ping      float     NOT NULL,
-  jitter    float     NOT NULL,
+  dl        float       NOT NULL,
+  ul        float       NOT NULL,
+  ping      float       NOT NULL,
+  jitter    float       NOT NULL,
   log       text,
-  uuid      tinytext  NOT NULL,
-  PRIMARY KEY (id)
+  uuid      tinytext    NOT NULL,
+  PRIMARY KEY (id),
+  KEY idx_speedtest_users_001 (ip)
 );
 CREATE USER IF NOT EXISTS '$DB_USERNAME'@'%' IDENTIFIED WITH mysql_native_password BY '$DB_PASSWORD';
 GRANT INSERT, SELECT ON $DB_DATABASE.speedtest_users TO '$DB_USERNAME'@'%';
