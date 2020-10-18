dc7878881be1af000d4d4fa97e094350780c1ebf
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-10-18 08:10:18 +0300

    Refactor the LibreSpeed data types
    https://github.com/librespeed/speedtest-go/issues/13

--- librespeed/init/initialize-database.sql
+++ librespeed/init/initialize-database.sql
@@ -1,19 +1,19 @@
 CREATE DATABASE $DB_DATABASE;
 USE $DB_DATABASE;
-CREATE TABLE speedtest_users( -- https://github.com/librespeed/speedtest-go/blob/master/database/mysql/telemetry_mysql.sql
+CREATE TABLE speedtest_users( -- Based on https://github.com/librespeed/speedtest-go/blob/master/database/mysql/telemetry_mysql.sql
   id        int(11)   NOT NULL AUTO_INCREMENT,
   timestamp timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  ip        text      NOT NULL,
+  ip        tinytext  NOT NULL, -- FIXME
   ispinfo   text,
   extra     text,
-  ua        text      NOT NULL,
-  lang      text      NOT NULL,
-  dl        text,
-  ul        text,
-  ping      text,
-  jitter    text,
-  log       longtext,
-  uuid      text,
+  ua        text,
+  lang      tinytext,
+  dl        float     NOT NULL,
+  ul        float     NOT NULL,
+  ping      float     NOT NULL,
+  jitter    float     NOT NULL,
+  log       text,
+  uuid      tinytext  NOT NULL,
   PRIMARY KEY (id)
 );
 CREATE USER IF NOT EXISTS '$DB_USERNAME'@'%' IDENTIFIED WITH mysql_native_password BY '$DB_PASSWORD';
