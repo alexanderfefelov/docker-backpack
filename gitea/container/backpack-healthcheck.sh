5c6847737727bc91cdce563b8becaa79f21b534f
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-11-07 05:10:47 +0300

    Fix the Gitea healthcheck

--- gitea/container/backpack-healthcheck.sh
+++ gitea/container/backpack-healthcheck.sh
@@ -8,5 +8,5 @@ readonly WAIT_TIMEOUT=2
 
 nc -z -w $WAIT_TIMEOUT localhost 22
 curl --head --fail --silent --max-time $WAIT_TIMEOUT --user-agent "$(basename "$0")" \
-  http://localhost:3000/ \
+  http://localhost:3001/ \
   > /dev/null
