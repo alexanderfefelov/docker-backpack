328cbe7921df2edf41b5dbff7e002836d95a8134
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-11-11 23:18:38 +0300

    Add JFrog Container Registry to the playground

---
+++ .playground/container-registry/README.md
@@ -0,0 +1,9 @@
+# backpack-container-registry
+
+Hostname `container-registry.backpack.test`
+
+## Services
+
+| Port | Service | Credentials
+| ---: | :------ | :----------
+| [8089](http://container-registry.backpack.test:8089) | HTTP API and management console | `admin` / `password`
---
+++ .playground/container-registry/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a pipeline, which may consist of a single simple command,
+# a list, or a compound command returns a non-zero status
+set -e
+
+# Elevate privileges
+[ $UID -eq 0 ] || exec sudo bash "$0" "$@"
+
+. settings.sh
+
+docker build --tag $IMAGE_NAME .
