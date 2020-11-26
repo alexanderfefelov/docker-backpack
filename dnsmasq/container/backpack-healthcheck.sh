1259e2f86764454d4f707221e9a33c5b187bcfa1
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-11-26 20:51:57 +0300

    Do not use hardcoded credentials

--- dnsmasq/container/backpack-healthcheck.sh
+++ dnsmasq/container/backpack-healthcheck.sh
@@ -8,5 +8,5 @@ readonly WAIT_TIMEOUT=2
 
 nc -z -w $WAIT_TIMEOUT -u localhost 53
 curl --head --fail --silent --max-time $WAIT_TIMEOUT --user-agent "$(basename "$0")" \
-  --user admin_inestorasyno:impardalwina http://localhost:8080/ \
+  --user $HTTP_USER:$HTTP_PASS http://localhost:8080/ \
   > /dev/null
