5b7d46e8a2a398b26db157723c18c978cb8f4043
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-11-29 19:13:36 +0300

    Do not use petname as a node name

--- message-queues/activemq/settings.sh
+++ message-queues/activemq/settings.sh
@@ -4,7 +4,7 @@ readonly IMAGE_NAME=alexanderfefelov/$BASE_NAME
 
 readonly NODE_1_CONTAINER_NAME=$BASE_NAME-node-1
 readonly NODE_1_HOST_NAME=$MONIKER-node-1.backpack.test
-readonly NODE_1_BROKER_NAME=$(petname)
+readonly NODE_1_BROKER_NAME=node-1
 readonly NODE_1_MQTT_PORT=1883
 readonly NODE_1_AMQP_PORT=5670
 readonly NODE_1_HTTP_PORT=8161
@@ -14,7 +14,7 @@ readonly NODE_1_OPENWIRE_PORT=61616
 
 readonly NODE_2_CONTAINER_NAME=$BASE_NAME-node-2
 readonly NODE_2_HOST_NAME=$MONIKER-node-2.backpack.test
-readonly NODE_2_BROKER_NAME=$(petname)
+readonly NODE_2_BROKER_NAME=node-2
 readonly NODE_2_MQTT_PORT=1884
 readonly NODE_2_AMQP_PORT=5671
 readonly NODE_2_HTTP_PORT=8162
@@ -24,7 +24,7 @@ readonly NODE_2_OPENWIRE_PORT=61619
 
 readonly NODE_3_CONTAINER_NAME=$BASE_NAME-node-3
 readonly NODE_3_HOST_NAME=$MONIKER-node-3.backpack.test
-readonly NODE_3_BROKER_NAME=$(petname)
+readonly NODE_3_BROKER_NAME=node-3
 readonly NODE_3_MQTT_PORT=1885
 readonly NODE_3_AMQP_PORT=5672
 readonly NODE_3_HTTP_PORT=8163
