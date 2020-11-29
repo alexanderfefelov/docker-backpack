9fcb330bb24779bc7e3ff8449edce548d8f63746
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-11-29 19:11:44 +0300

    Add Moodle to the playground

---
+++ .playground/moodle/settings.sh
@@ -0,0 +1,11 @@
+readonly MOODLE_VERSION=3.10.0
+
+readonly MONIKER=moodle
+readonly BASE_NAME=backpack-$MONIKER
+readonly IMAGE_NAME=alexanderfefelov/$BASE_NAME
+readonly CONTAINER_NAME=$BASE_NAME
+readonly HOST_NAME=$MONIKER.backpack.test
+readonly WAIT_TIMEOUT=600
+
+. ../../lib/settings/health.sh
+. ../../lib/settings/log.sh
