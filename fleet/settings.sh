c413866d33effd7b57d271d1bc3d61b3f30675e2
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-12-15 14:22:52 +0300

    Refactoring

--- fleet/settings.sh
+++ fleet/settings.sh
@@ -1,3 +1,6 @@
+readonly DEFAULT_VERSION=3.5.1
+export VERSION=${VERSION:-$DEFAULT_VERSION}
+
 readonly MONIKER=fleet
 readonly BASE_NAME=backpack-$MONIKER
 readonly IMAGE_NAME=alexanderfefelov/$BASE_NAME
