dd03d48b4e71e9242961652d57d679bf175b9f13
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-12-16 16:39:43 +0300

    Refactoring

--- swagger/ui/settings.sh
+++ swagger/ui/settings.sh
@@ -1,3 +1,6 @@
+readonly DEFAULT_VERSION=3.37.2
+export VERSION=${VERSION:-$DEFAULT_VERSION}
+
 readonly MONIKER=swagger-ui
 readonly BASE_NAME=backpack-$MONIKER
 readonly IMAGE_NAME=alexanderfefelov/$BASE_NAME
