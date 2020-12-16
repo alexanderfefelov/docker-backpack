cc7ab5ca054857a69d433d5e73081bcae8818efc
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-12-16 17:08:34 +0300

    Refactoring

--- mattermost/settings.sh
+++ mattermost/settings.sh
@@ -1,3 +1,6 @@
+readonly DEFAULT_VERSION=5.29.1
+export VERSION=${VERSION:-$DEFAULT_VERSION}
+
 readonly MONIKER=mattermost
 readonly BASE_NAME=backpack-$MONIKER
 readonly IMAGE_NAME=alexanderfefelov/$BASE_NAME
