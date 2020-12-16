36796c4923defd29f258eae751097099afb5b537
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-12-16 17:56:52 +0300

    Refactoring

--- network-monitoring/the-dude/settings.sh
+++ network-monitoring/the-dude/settings.sh
@@ -1,3 +1,6 @@
+readonly DEFAULT_VERSION=4.0beta3-1
+export VERSION=${VERSION:-$DEFAULT_VERSION}
+
 readonly MONIKER=the-dude
 readonly BASE_NAME=backpack-$MONIKER
 readonly IMAGE_NAME=alexanderfefelov/$BASE_NAME
