fe9dc05fa9196af17b925b8e3c9c77758e6a07bc
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-12-16 16:53:56 +0300

    Refactoring

--- netdata/run.sh
+++ netdata/run.sh
@@ -5,7 +5,7 @@
 set -e
 
 # Elevate privileges
-[ $UID -eq 0 ] || exec sudo bash "$0" "$@"
+[ $UID -eq 0 ] || exec sudo --preserve-env=VERSION bash "$0" "$@"
 
 . settings.sh
 . ../lib/lib.sh
@@ -27,7 +27,7 @@ run() {
     --publish 19999:19999 \
     --env DOCKER_USR=root \
     $DEFAULT_LOG_SETTINGS \
-    $IMAGE_NAME
+    $IMAGE_NAME:$VERSION
 }
 
 run
--- netdata/settings.sh
+++ netdata/settings.sh
@@ -1,3 +1,6 @@
+readonly DEFAULT_VERSION=1.26.0
+export VERSION=${VERSION:-$DEFAULT_VERSION}
+
 readonly MONIKER=netdata
 readonly BASE_NAME=backpack-$MONIKER
 readonly IMAGE_NAME=alexanderfefelov/$BASE_NAME
