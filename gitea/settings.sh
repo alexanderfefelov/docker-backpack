e506b912b999ce4e92f8997cddeafe51765cd5fd
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-12-16 17:15:56 +0300

    Refactoring

--- gitea/settings.sh
+++ gitea/settings.sh
@@ -1,3 +1,6 @@
+readonly DEFAULT_VERSION=1.13.0
+export VERSION=${VERSION:-$DEFAULT_VERSION}
+
 readonly MONIKER=gitea
 readonly BASE_NAME=backpack-$MONIKER
 readonly IMAGE_NAME=alexanderfefelov/$BASE_NAME
