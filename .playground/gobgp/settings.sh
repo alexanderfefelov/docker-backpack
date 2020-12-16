6b12022ef203a29aa087b8428d567f5e74e84ddb
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-12-16 16:05:01 +0300

    Refactoring

--- .playground/gobgp/settings.sh
+++ .playground/gobgp/settings.sh
@@ -1,3 +1,6 @@
+readonly DEFAULT_VERSION=2.22.0
+export VERSION=${VERSION:-$DEFAULT_VERSION}
+
 readonly MONIKER=gobgp
 readonly BASE_NAME=backpack-$MONIKER
 readonly IMAGE_NAME=alexanderfefelov/$BASE_NAME
