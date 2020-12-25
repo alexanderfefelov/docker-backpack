2c6c6d41339646c6631f4053920aeacae8a51949
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-12-25 23:00:54 +0300

    Start working on Minecraft
    See #20

---
+++ minecraft/container/download-server.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+readonly VERSION=$1
+readonly CURL="curl --silent --fail --show-error"
+readonly URL_VERSIONS_MANIFEST=https://launchermeta.mojang.com/mc/game/version_manifest.json
+echo Versions manifest URL: $URL_VERSIONS_MANIFEST
+readonly URL_RELEASE_MANIFEST=$($CURL $URL_VERSIONS_MANIFEST \
+  | jq --raw-output '.versions[] | select( .id | contains("'$VERSION'")) | select( .type | contains("release")) | .url')
+echo Release manifest URL: $URL_RELEASE_MANIFEST
+readonly URL_SERVER=$($CURL $URL_RELEASE_MANIFEST \
+  | jq --raw-output .downloads.server.url)
+echo Server URL: $URL_SERVER
+$CURL --output server.jar $URL_SERVER
