0cbb5817a720f9cdd81b3be4c9521fb34b1bc0ac
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-12-25 23:29:03 +0300

    Refactoring

--- minecraft/container/download-server.sh
+++ minecraft/container/download-server.sh
@@ -5,7 +5,7 @@ readonly CURL="curl --silent --fail --show-error"
 readonly URL_VERSIONS_MANIFEST=https://launchermeta.mojang.com/mc/game/version_manifest.json
 echo Versions manifest URL: $URL_VERSIONS_MANIFEST
 readonly URL_RELEASE_MANIFEST=$($CURL $URL_VERSIONS_MANIFEST \
-  | jq --raw-output '.versions[] | select( .id | contains("'$VERSION'")) | select( .type | contains("release")) | .url')
+  | jq --raw-output '.versions[] | select(.id == "'$VERSION'") | select(.type == "release") | .url')
 echo Release manifest URL: $URL_RELEASE_MANIFEST
 readonly URL_SERVER=$($CURL $URL_RELEASE_MANIFEST \
   | jq --raw-output .downloads.server.url)
