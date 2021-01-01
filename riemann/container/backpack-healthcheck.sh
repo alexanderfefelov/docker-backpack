0adfc074a6ea371bb886eaef70756309ead65ada
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-01-02 01:06:17 +0300

    Check health of Riemann

---
+++ riemann/container/backpack-healthcheck.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Exit immediately if a pipeline, which may consist of a single simple command,
+# a list, or a compound command returns a non-zero status
+set -e
+
+readonly WAIT_TIMEOUT=2
+
+nc -z -w $WAIT_TIMEOUT localhost 5555
+nc -z -w $WAIT_TIMEOUT -u localhost 5555
+nc -z -w $WAIT_TIMEOUT localhost 5556
