119089ef2a7f67ad87c15cb21dbe0f84ff6e775f
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-01-24 07:29:27 +0300

    Check health of Kala

---
+++ workload-automation/kala/container/target/backpack-healthcheck.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Exit immediately if a pipeline, which may consist of a single simple command,
+# a list, or a compound command returns a non-zero status
+set -e
+
+readonly WAIT_TIMEOUT=2
+
+curl --fail --silent --max-time $WAIT_TIMEOUT --user-agent "$(basename "$0")" \
+  http://localhost:8000/api/v1/stats/ \
+  > /dev/null
