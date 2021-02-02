b0398a822629f93846924ba1cf4ac3efd4ba86a3
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-02-02 19:24:01 +0300

    9300 is a TCP transport, not HTTP
    Fix #37

--- elastic/elasticsearch/container/backpack-healthcheck.sh
+++ elastic/elasticsearch/container/backpack-healthcheck.sh
@@ -9,6 +9,4 @@ readonly WAIT_TIMEOUT=2
 curl --head --fail --silent --max-time $WAIT_TIMEOUT --user-agent "$(basename "$0")" \
   http://healthcheck_sheetheekahx:yoongootecie@localhost:9200/ \
   > /dev/null
-curl --head --fail --silent --max-time $WAIT_TIMEOUT --user-agent "$(basename "$0")" \
-  http://healthcheck_sheetheekahx:yoongootecie@localhost:9300/ \
-  > /dev/null
+nc -z -w $WAIT_TIMEOUT localhost 9300
