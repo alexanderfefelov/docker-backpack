5f232c55c9d4e741a05cfdb5c6f1a1f3b2eb7e4d
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-02-03 08:09:03 +0300

    Add a few utils for NAV

--- network-monitoring/nav/utils/lib.sh
+++ network-monitoring/nav/utils/lib.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/env bash
 
 readonly DOCKER_EXEC="docker exec --tty --interactive backpack-nav"
+
+readonly HTTP="http --check-status"
+readonly API=http://nav.backpack.test:83/api/1
+readonly TOKEN=aer9tei2ohsheiv5cae2ohCh3biz6kaf2pieDi0gee
+
+#
+# Arguments:
+#   $1 - significant part of the API URL
+# Returns:
+#   response body
+#
+execute_get_request() {
+  local response=$(
+    $HTTP --body \
+      GET $API/$1 \
+        "Authorization: Token $TOKEN"
+  )
+  echo $response
+}
