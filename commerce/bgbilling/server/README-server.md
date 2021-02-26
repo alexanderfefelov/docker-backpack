829b92dd643cc2bb7f301cf1d768b3f58c7bab11
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-02-26 07:22:58 +0300

    Create an account for demo/example purposes

--- commerce/bgbilling/server/README-server.md
+++ commerce/bgbilling/server/README-server.md
@@ -8,6 +8,7 @@ Hostname `bgbilling-server.backpack.test`
 | ---: | :------ | :----------
 | [8098](http://bgbilling-server.backpack.test:8098) | BGBilling HTTP API | `admin_fu4aixied1eo` / `ufumeing4aic`
 | | | `api_fah4iew1idie` / `iepec4jee2lo`
+| | | `foobar_Ee0ahm9shoht` / `uaz3ji4uQuie`
 | | | `installer_gaedeerei4wi` / `ai6is1tae3ya`
 | 8098/udp | BGBilling `AdminPortListener` | -
 | [8099](http://bgbilling-server.backpack.test:8099) | Jolokia HTTP [API](https://jolokia.org/reference/html/protocol.html) | `jolokia_jei4cie5Kahg` / `eaFai4eTh1re`
--- commerce/bgbilling/server/init/configure-accounts/create-accounts.sql
+++ commerce/bgbilling/server/init/configure-accounts/create-accounts.sql
@@ -5,4 +5,5 @@ ALTER TABLE `user` auto_increment = 200;
 INSERT INTO `user`(id, login, pswd, email, gr, crm_user_id, dt, `name`, descr, config) VALUES
   ( 42, 'installer_gaedeerei4wi', upper(md5('ai6is1tae3ya')), '', 0, 0, now(), 'Инсталлятор',   '', ''),
   (100, 'api_fah4iew1idie',       upper(md5('iepec4jee2lo')), '', 0, 0, now(), 'API',           'It''s Okay to say "I don''t know!" There''s no shame in that! The only shame is to pretend that we know everything.', ''),
-  (101, 'admin_fu4aixied1eo',     upper(md5('ufumeing4aic')), '', 0, 0, now(), 'Администратор', 'Bluetooth Ninja', '');
+  (101, 'admin_fu4aixied1eo',     upper(md5('ufumeing4aic')), '', 0, 0, now(), 'Администратор', 'Bluetooth Ninja', ''),
+  (102, 'foobar_Ee0ahm9shoht',    upper(md5('uaz3ji4uQuie')), '', 0, 0, now(), '', '', '');
