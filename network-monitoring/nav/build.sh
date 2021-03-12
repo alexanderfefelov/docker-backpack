66602fd76202dbea36583b09f04cc7407cd52cce
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-03-12 16:23:16 +0300

    Pin the nav-add-ons version

--- network-monitoring/nav/build.sh
+++ network-monitoring/nav/build.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 # Elevate privileges
-[ $UID -eq 0 ] || exec sudo --preserve-env=VERSION bash "$0" "$@"
+[ $UID -eq 0 ] || exec sudo --preserve-env=VERSION --preserve-env=ADD_ONS_VERSION bash "$0" "$@"
 
 . settings.sh
 
 export IMAGE_NAME
 docker build \
   --build-arg VERSION \
+  --build-arg ADD_ONS_VERSION \
   --build-arg IMAGE_NAME \
   --tag $IMAGE_NAME:$VERSION \
   .
