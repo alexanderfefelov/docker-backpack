0927916ec0acef70a7f087fef50cd9df63ada091
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-03-13 06:23:49 +0300

    Follow the nav-add-ons changes

--- network-monitoring/nav/container/builder/apply-patches.sh
+++ network-monitoring/nav/container/builder/apply-patches.sh
@@ -5,9 +5,10 @@
 set -e
 
 readonly NAV_HOME=/.build/usr/local/lib/python3.7/site-packages/nav
-readonly NAV_VERSION=36819df
 
-patch $NAV_HOME/ipdevpoll/plugins/sensors.py    /nav-add-ons/snmp/patches/nav/ipdevpoll/plugins/sensors.py-$NAV_VERSION.patch
-patch $NAV_HOME/ipdevpoll/plugins/statsystem.py /nav-add-ons/snmp/patches/nav/ipdevpoll/plugins/statsystem.py-$NAV_VERSION.patch
-patch $NAV_HOME/models/manage.py                /nav-add-ons/web/patches/nav/models/manage.py-$NAV_VERSION.patch
-patch $NAV_HOME/web/info/searchproviders.py     /nav-add-ons/web/patches/nav/web/info/searchproviders.py-$NAV_VERSION.patch
+patch $NAV_HOME/ipdevpoll/plugins/sensors.py    /nav-add-ons/snmp/patches/nav/ipdevpoll/plugins/sensors.py-36819df.patch
+patch $NAV_HOME/ipdevpoll/plugins/statsystem.py /nav-add-ons/snmp/patches/nav/ipdevpoll/plugins/statsystem.py-36819df.patch
+patch $NAV_HOME/ipdevpoll/shadows/__init__.py   /nav-add-ons/snmp/patches/nav/ipdevpoll/shadows/__init__.py-9615ee5.patch
+
+patch $NAV_HOME/models/manage.py                /nav-add-ons/web/patches/nav/models/manage.py-36819df.patch
+patch $NAV_HOME/web/info/searchproviders.py     /nav-add-ons/web/patches/nav/web/info/searchproviders.py-36819df.patch
