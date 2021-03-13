1000d378184569ea34799b646ef6387d20a126ad
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-03-13 08:58:52 +0300

    Follow the nav-add-ons changes
    and fix some waste.

--- network-monitoring/nav/container/builder/apply-patches.sh
+++ network-monitoring/nav/container/builder/apply-patches.sh
@@ -6,9 +6,10 @@ set -e
 
 readonly NAV_HOME=/.build/usr/local/lib/python3.7/site-packages/nav
 
+patch $NAV_HOME/ipdevpoll/plugins/interfaces.py /nav-add-ons/snmp/patches/nav/ipdevpoll/plugins/interfaces.py-90d9a11.patch
 patch $NAV_HOME/ipdevpoll/plugins/sensors.py    /nav-add-ons/snmp/patches/nav/ipdevpoll/plugins/sensors.py-36819df.patch
 patch $NAV_HOME/ipdevpoll/plugins/statsystem.py /nav-add-ons/snmp/patches/nav/ipdevpoll/plugins/statsystem.py-36819df.patch
-patch $NAV_HOME/ipdevpoll/shadows/__init__.py   /nav-add-ons/snmp/patches/nav/ipdevpoll/shadows/__init__.py-9615ee5.patch
+patch $NAV_HOME/ipdevpoll/shadows/__init__.py   /nav-add-ons/snmp/patches/nav/ipdevpoll/shadows/__init__.py-90d9a11.patch
 
 patch $NAV_HOME/models/manage.py                /nav-add-ons/web/patches/nav/models/manage.py-36819df.patch
 patch $NAV_HOME/web/info/searchproviders.py     /nav-add-ons/web/patches/nav/web/info/searchproviders.py-36819df.patch
