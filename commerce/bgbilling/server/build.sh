67e1f3c2098b199a7e967d0d51d62a55221e33bc
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-03-30 12:55:42 +0300

    Fail fast

--- commerce/bgbilling/server/build.sh
+++ commerce/bgbilling/server/build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Exit immediately if a pipeline, which may consist of a single simple command,
+# a list, or a compound command returns a non-zero status
+set -e
+
 # Elevate privileges
 [ $UID -eq 0 ] || exec sudo --preserve-env=VERSION bash "$0" "$@"
 
