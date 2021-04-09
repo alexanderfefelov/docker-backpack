0debe1d6b092e53c96806ffc7f474228d91f3827
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-04-09 16:23:50 +0300

    Make use of the Employees database (cont.)

---
+++ databases/mysql/main/utils/test-database/load.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# https://dev.mysql.com/doc/employee/en/
+
+TEST_DB_REPO=test_db
+TEST_DB_URL=https://github.com/datacharmer/$TEST_DB_REPO
+TEST_DB_VERSION=v1.0.7
+TEST_DB_RESTORE_SCRIPT=employees.sql
+
+if [ ! -d $TEST_DB_REPO ]; then
+  git clone --quiet $TEST_DB_URL --branch $TEST_DB_VERSION
+fi
+
+cd $TEST_DB_REPO
+time ../cli-master.sh --table < $TEST_DB_RESTORE_SCRIPT
+cd ..
---
+++ databases/mysql/main/utils/test-database/test-on-slave.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+TEST_DB_REPO=test_db
+TEST_DB_TEST_SCRIPT=test_employees_md5.sql
+
+[ ! -d $TEST_DB_REPO ] && echo $TEST_DB_REPO not found && exit 1
+
+cd $TEST_DB_REPO
+time ../cli-slave.sh --table < $TEST_DB_TEST_SCRIPT
+cd ..
