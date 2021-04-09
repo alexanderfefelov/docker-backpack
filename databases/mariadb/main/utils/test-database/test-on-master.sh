f7578bab5918d50255bb283bc321a266b89aa796
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-04-09 17:26:50 +0300

    Make use of the Employees database (cont.)

---
+++ databases/mariadb/main/utils/test-database/test-on-master.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+TEST_DB_REPO=test_db
+TEST_DB_TEST_SCRIPT=test_employees_md5.sql
+
+[ ! -d $TEST_DB_REPO ] && echo $TEST_DB_REPO not found && exit 1
+
+(
+  cd $TEST_DB_REPO
+  time ../../cli-master.sh --table < $TEST_DB_TEST_SCRIPT
+)
