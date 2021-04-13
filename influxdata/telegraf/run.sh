0196700656860373839cbf0c7f07a668b7dd4caa
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-04-13 06:11:54 +0300

    Use host networking from Telegraf
    Fix #76

--- influxdata/telegraf/run.sh
+++ influxdata/telegraf/run.sh
@@ -15,6 +15,7 @@ run() {
     --name $CONTAINER_NAME \
     --hostname $HOST_NAME \
     --detach \
+    --network host \
     --volume /etc/localtime:/etc/localtime:ro --volume /etc/timezone:/etc/timezone:ro \
     --volume $CONTAINER_NAME-conf:/etc/telegraf \
     --volume /var/run/docker.sock:/var/run/docker.sock \
