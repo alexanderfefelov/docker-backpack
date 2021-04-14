fd02d25672705f254a74f7a5144f29f41e583cd9
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-04-14 06:00:05 +0300

    Cleanup

--- business-intelligence/jasperreports/init/initialize-jasperreports.sh
+++ business-intelligence/jasperreports/init/initialize-jasperreports.sh
@@ -2,7 +2,6 @@ readonly CONTAINER_NAME=$1
 
 docker exec --tty --interactive $CONTAINER_NAME bash -c '
   set -e
-  mkdir --parents $ks $ksp
   cd /dist/buildomatic
   ./js-ant init-js-db-ce
   ./js-ant import-minimal-ce
