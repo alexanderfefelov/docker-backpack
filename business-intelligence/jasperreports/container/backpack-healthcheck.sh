bdc866074ee5c62d3926ca900de78a9865532fdd
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-04-22 04:49:32 +0300

    Check health of Jolokia

--- business-intelligence/jasperreports/container/backpack-healthcheck.sh
+++ business-intelligence/jasperreports/container/backpack-healthcheck.sh
@@ -5,3 +5,6 @@ readonly WAIT_TIMEOUT=2
 curl --head --fail --silent --max-time $WAIT_TIMEOUT --user-agent "$(basename "$0")" \
   http://localhost:8080/jasperserver \
   > /dev/null
+curl --fail --silent --max-time $WAIT_TIMEOUT --user-agent "$(basename "$0")" \
+  http://jolokia_ahkahshoog5u:niedohque9yu@localhost:8778/jolokia/ \
+  > /dev/null
