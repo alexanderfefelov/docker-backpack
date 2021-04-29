be5bd991a32458cd9a37137b31a73fe77417e302
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-04-29 21:25:25 +0300

    Create custom fields in Redmine

---
+++ bug-tracking/redmine/init/configure-redmine/create-custom-fields.sql
@@ -0,0 +1,4 @@
+INSERT INTO custom_fields (type, name, field_format, searchable, is_filter, is_for_all) VALUES
+  ('UserCustomField', 'external_system_id', 'string', true, true, false),
+  ('ProjectCustomField', 'external_system_id', 'string', true, true, false),
+  ('IssueCustomField', 'external_system_id', 'string', true, true, true);
