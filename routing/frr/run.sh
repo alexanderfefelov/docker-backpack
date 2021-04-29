0165facb8806d6bcfca1e36823b665fb31a5a212
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-04-29 14:52:24 +0300

    Run FRR in privileged mode

--- routing/frr/run.sh
+++ routing/frr/run.sh
@@ -15,6 +15,7 @@ run() {
     --name $CONTAINER_NAME \
     --hostname $HOST_NAME \
     --detach \
+    --privileged \
     --volume /etc/localtime:/etc/localtime:ro --volume /etc/timezone:/etc/timezone:ro \
     --volume $CONTAINER_NAME-conf:/etc/frr \
     --volume $CONTAINER_NAME-log:/var/log/frr \
