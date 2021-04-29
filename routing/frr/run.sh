2ed8c464b16e5321a6b910d42f94a996ba158f8b
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-04-29 10:43:20 +0300

    Enable staticd logging

--- routing/frr/run.sh
+++ routing/frr/run.sh
@@ -17,6 +17,7 @@ run() {
     --detach \
     --volume /etc/localtime:/etc/localtime:ro --volume /etc/timezone:/etc/timezone:ro \
     --volume $CONTAINER_NAME-conf:/etc/frr \
+    --volume $CONTAINER_NAME-log:/var/log/frr \
     $DEFAULT_HEALTH_SETTINGS \
     $DEFAULT_LOG_SETTINGS \
     $IMAGE_NAME:$VERSION
