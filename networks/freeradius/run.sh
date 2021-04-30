9fc9d672a81270fb1d70c1afb92a52ecf1180a4f
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-04-30 16:52:40 +0300

    WIP FreeRADIUS

--- networks/freeradius/run.sh
+++ networks/freeradius/run.sh
@@ -16,6 +16,10 @@ run() {
     --hostname $HOST_NAME \
     --detach \
     --volume /etc/localtime:/etc/localtime:ro --volume /etc/timezone:/etc/timezone:ro \
+    --volume $CONTAINER_NAME-conf:/etc/freeradius \
+    --volume $CONTAINER_NAME-conf:/var/log/freeradius \
+    --publish 1812:1812/udp \
+    --publish 1813:1813/udp \
     $DEFAULT_HEALTH_SETTINGS \
     $DEFAULT_LOG_SETTINGS \
     $IMAGE_NAME:$VERSION
