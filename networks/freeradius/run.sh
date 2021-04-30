3baa8ea66c6cd17c14cba29607d9709eeffe94dd
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-04-30 17:00:17 +0300

    Haste makes waste

--- networks/freeradius/run.sh
+++ networks/freeradius/run.sh
@@ -17,7 +17,7 @@ run() {
     --detach \
     --volume /etc/localtime:/etc/localtime:ro --volume /etc/timezone:/etc/timezone:ro \
     --volume $CONTAINER_NAME-conf:/etc/freeradius \
-    --volume $CONTAINER_NAME-conf:/var/log/freeradius \
+    --volume $CONTAINER_NAME-log:/var/log/freeradius \
     --publish 1812:1812/udp \
     --publish 1813:1813/udp \
     $DEFAULT_HEALTH_SETTINGS \
