8ff802981f98f37c22508790e05d4f14258355b2
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-05-14 08:37:41 +0300

    Refactoring

---
+++ workload-automation/dkron/server/show-instance-logs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Elevate privileges
+[ $UID -eq 0 ] || exec sudo bash "$0" "$@"
+
+. settings.sh
+
+INSTANCE_NO=${1?Instance number is not specified}
+CONTAINER_NAME=SERVER_${INSTANCE_NO}_CONTAINER_NAME
+
+docker logs --follow ${!CONTAINER_NAME}
--- workload-automation/dkron/server/show-logs-server-3.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Elevate privileges
-[ $UID -eq 0 ] || exec sudo bash "$0" "$@"
-
-. settings.sh
-
-docker logs --follow $SERVER_3_CONTAINER_NAME
