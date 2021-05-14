e55b4fd66eab1cff5ecbb26f86cd95fb4e7df912
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-05-14 21:40:15 +0300

    Minor fix

--- workload-automation/dkron/agent/clear-instance-logs.sh
+++ workload-automation/dkron/agent/clear-instance-logs.sh
@@ -5,7 +5,7 @@
 
 . settings.sh
 
-INSTANCE_NO=${1?Instance number is not specified}
-CONTAINER_NAME=AGENT_${INSTANCE_NO}_CONTAINER_NAME
+readonly INSTANCE_NO=${1?Instance number is not specified}
+readonly CONTAINER_NAME=AGENT_${INSTANCE_NO}_CONTAINER_NAME
 
 > $(docker inspect --format='{{.LogPath}}' ${!CONTAINER_NAME})
