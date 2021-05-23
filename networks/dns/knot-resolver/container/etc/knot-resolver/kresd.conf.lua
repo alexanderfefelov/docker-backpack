573c6f85208a8fc93d5f6ac2746d50c7fb186ae7
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-05-23 07:41:41 +0300

    Fix code format

--- networks/dns/knot-resolver/container/etc/knot-resolver/kresd.conf.lua
+++ networks/dns/knot-resolver/container/etc/knot-resolver/kresd.conf.lua
@@ -3,10 +3,10 @@ modules = {
   'http'
 }
 
-net.listen('0.0.0.0', 53, { kind = 'dns' })
-net.listen('0.0.0.0', 443, { kind = 'doh2' })
-net.listen('0.0.0.0', 853, { kind = 'tls' })
-net.listen('0.0.0.0', 8453, { kind = 'webmgmt' })
+net.listen('0.0.0.0', 53, {kind = 'dns'})
+net.listen('0.0.0.0', 443, {kind = 'doh2'})
+net.listen('0.0.0.0', 853, {kind = 'tls'})
+net.listen('0.0.0.0', 8453, {kind = 'webmgmt'})
 
 hints.add_hosts('/etc/knot-resolver/hosts.generated')
 
