ac7a251238c629cef1f43adce233aa4ae532ead0
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-05-27 05:42:47 +0300

    Check required containers first

--- commerce/bgbilling/server/run-server.sh
+++ commerce/bgbilling/server/run-server.sh
@@ -60,6 +60,8 @@ run() {
     $IMAGE_NAME-server:$VERSION
 }
 
+check_containers "$REQUIRED_CONTAINERS" || exit 1
+
 $MYSQL --execute="USE $DB_DATABASE;"
 readonly USE_DB_RETCODE=$?
 
@@ -73,7 +75,6 @@ if [ "$USE_DB_RETCODE" -ne 0 ]; then
   execute_scripts init/configure-accounts
 fi
 
-check_containers "$REQUIRED_CONTAINERS"
 run
 wait_for_all_container_ports $SERVER_CONTAINER_NAME $WAIT_TIMEOUT
 print_container_info $SERVER_CONTAINER_NAME
