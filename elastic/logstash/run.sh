2e288615feb41f2351926862e148cc79731f71cf
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-05-28 14:22:58 +0300

    Haste makes waste

--- elastic/logstash/run.sh
+++ elastic/logstash/run.sh
@@ -16,7 +16,8 @@ run() {
     --hostname $HOST_NAME \
     --detach \
     --volume /etc/localtime:/etc/localtime:ro --volume /etc/timezone:/etc/timezone:ro \
-    --volume $CONTAINER_NAME-conf:/usr/share/logstash \
+    --volume $CONTAINER_NAME-conf:/usr/share/logstash/config \
+    --volume $CONTAINER_NAME-pipeline:/usr/share/logstash/pipelines \
     --publish 5514:5514 \
     --publish 5514:5514/udp \
     --publish 9600:9600 \
