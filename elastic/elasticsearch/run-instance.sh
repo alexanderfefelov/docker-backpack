8fc45f93d34ecea3f284b939c9263686dc870b91
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-06-12 04:32:23 +0300

    Refactoring

--- elastic/elasticsearch/run-instance.sh
+++ elastic/elasticsearch/run-instance.sh
@@ -19,36 +19,6 @@ readonly TRANSPORT_PORT=NODE_${INSTANCE_NO}_TRANSPORT_PORT
 . ../../lib/lib.sh
 . functions.sh
 
-check_vm_max_map_count() {
-  local -r PARAM=vm.max_map_count
-  local -r REQUIRED_MIN=262144
-  local -r CURRENT=$(sysctl $PARAM | awk '{ print $3 }')
-  if [ $CURRENT -lt $REQUIRED_MIN ]; then
-    echo Current value for $PARAM is $CURRENT, minimum required value is $REQUIRED_MIN >&2
-    exit 1
-  fi
-}
-
-readonly SSE_SUPPORT=$(cat /proc/cpuinfo | grep flags | grep --only-matching --word-regexp 'sse\S*' | sort | uniq | paste --delimiters=' ' --serial -)
-
-print_sse_info() {
-  echo Elasticsearch Machine Learning requires CPU with SSE4.2 support
-  if [ -z "$SSE_SUPPORT" ]; then
-    echo No SSE support detected
-  else
-    echo Detected CPU with support for $SSE_SUPPORT
-  fi
-}
-
-initialize_elasticsearch() {
-  if http --check-status GET http://$NODE_1_HOST_NAME:$NODE_1_HTTP_PORT --auth-type basic --auth installer_uru6oushi7oh:sohsah0oghat > /dev/null 2>&1; then
-    echo Initializing Elasticsearch...
-    bash init/configure-roles.sh
-    bash init/configure-accounts.sh $CONTAINER_NAME
-    echo ...Elasticsearch initialized
-  fi
-}
-
 check_vm_max_map_count
 print_sse_info
 run_instance \
